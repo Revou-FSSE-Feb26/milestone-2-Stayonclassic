a3a3b17f690396871f6c18de6564f676b2958607
Stayonclassic <leonardus.rc@gmail.com>
2026-04-30 19:07:01 +0700

    modefied minor js script

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -202,3 +202,12 @@ if (clearLeaderboardButton) {
     }
   });
 }
+
+// ===== Scroll to Top Button =====
+    const scrollTopButton = document.getElementById("scroll-top-btn");
+
+    if (scrollTopButton) {
+      scrollTopButton.addEventListener("click", () => {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+      });
+    }
--- js/scroll-top.js
+++
@@ -1,7 +0,0 @@
-    const scrollTopButton = document.getElementById("scroll-top-btn");
-
-    if (scrollTopButton) {
-      scrollTopButton.addEventListener("click", () => {
-        window.scrollTo({ top: 0, behavior: "smooth" });
-      });
-    }
